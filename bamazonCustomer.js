6e0e27bfcc677542b8409955582fbc54310df87e
Mariana <mariana.diaz@live.com>
2020-03-11 22:02:28 -0400

    add screenshots, create display products function, modify user prompt, add check stock function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,54 +1,92 @@
-var inquirer = require ('inquirer');
-var mysql = require ('mysql');
+var inquirer = require('inquirer');
+var mysql = require('mysql');
+pry = require('pryjs');
 
 var connection = mysql.createConnection({
-    host: "localhost",
-  
-    port: 3306,
-  
-    user: "root",
+  host: "localhost",
 
-    password: "Newcoder!9",
-    database: "bamazon"
-  });
-  
-  connection.connect(function(err) {
+  port: 3306,
+
+  user: "root",
+
+  password: "Newcoder!9",
+  database: "bamazon"
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("Welcome to Bamazon");
+  displayAllProducts();
+
+});
+
+function displayAllProducts() {
+  var query = "SELECT item_id, product_name, price FROM products";
+  connection.query(query, function (err, res) {
     if (err) throw err;
-    enterId();
-  });
+    for (var i = 0; i < res.length; i++) {
+      console.log("Product ID: " + res[i].item_id + " \nProduct Name: " + res[i].product_name + "\nPrice: " + "$" + res[i].price +"\n");
 
-  function enterId() {
-    inquirer
-      .prompt({
+    }
+    enterProduct();
+  })
+}
+
+
+
+function enterProduct() {
+  inquirer
+    .prompt([
+      {
         name: "id",
         type: "input",
         message: "Please enter the ID of the product you wish to purchase."
-      })
-      .then(function(answer) {
-        var query = "SELECT item_id, product_name, price, stock_quantity FROM products WHERE ?";
-        connection.query(query, {item_id: answer.id }, function(err, res){
-        for (var i = 0; i < res.length; i++) {
-            console.log(" Product ID: " + res[i].item_id + " /nProduct Name: " + res[i].product_name + " || Price: " + res[i].price);
-           console.log ("Quantity Available: " + res[i].stock_quantity); 
-          }
-        }) 
-        enterQuantity();
-  })
-}
-
-function enterQuantity() {
-  inquirer.prompt({
-    name: "quantity",
-    type: "input",
-    message: "Please enter quantity desired for this product."
-  })
-  .then(function(answer){
-    var query = "SELECT stock_quantity FROM products WHERE ?";
-    connection.query(query, {stock_quantity: answer.quantity}, function(err, res){
-      for (var i = 0; i < res.length; i++) {
-        console.log("Quantity Remaining: " + res[i].stock_quantity);
+      },
+      {
+        name: "quantity",
+        type: "input",
+        message: "Please enter quantity desired for this product."
       }
+    ]).then(function (answer) {
+      var query = "SELECT item_id, product_name, price, stock_quantity FROM products WHERE ?";
+      connection.query(query, { item_id: answer.id }, function (err, res) {
+        for (var i = 0; i < res.length; i++) {
+          var id = res[i].item_id;
+          console.log("Product ID: " + id + " \nProduct Name: " + res[i].product_name + "\nPrice: " + "$" + res[i].price);
+        }
+        var checkStock = checkIfInStock(answer.quantity, res[0].stock_quantity);
+        if (checkStock === true) {
+          connection.query(
+            "Update products SET stock_quantity = ? WHERE item_id = ?",
+            [
+              res[0].stock_quantity - answer.quantity, answer.id
+            ],
+            function (err, res) {
+              if (err) throw err;
+            }
+          )
+          console.log("Amount Due: " + "$" + res[0].price);
+          connection.end();
+        } else {
+          console.log("Insufficient Quantity!");
+          connection.end();
+        }
+
+
+      })
 
     })
-  })
+
 }
+
+function checkIfInStock(answerQuantity, stockQuantity) {
+  if (stockQuantity - answerQuantity > 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+
+
